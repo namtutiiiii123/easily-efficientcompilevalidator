410e8643be64fb5572ec3d9cc4c4274f82779df9
namtutiiiii123 <techmedboynam1311@outlook.com>
2024-05-13 18:10:29 +0700

    update sdk project

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-function combinationSum2(candidates, target) {
-  candidates.sort((a, b) => a - b);
-  const result = [];
-  backtrack([], 0, 0);
-  return result;
-  function backtrack(combination, start, sum) {
-    if (sum === target) {
-      result.push([...combination]);
-      return;
-    }
-    if (sum > target) return;
-    for (let i = start; i < candidates.length; i++) {
-      if (i > start && candidates[i] === candidates[i - 1]) continue;
-      combination.push(candidates[i]);
-      backtrack(combination, i + 1, sum + candidates[i]);
-      combination.pop();
-    }
+function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there are elements remaining
+  while (currentIndex !== 0) {
+    // Pick a remaining element
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // Swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
   }
+
+  return array;
 }
+
+const shuffledDeck = shuffle([1, 2, 3, 4, 5]);
+console.log(shuffledDeck);
