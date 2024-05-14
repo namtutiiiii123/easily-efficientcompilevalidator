c41c434bb52aa401145489bab8ca62d2709b6021
namtutiiiii123 <techmedboynam1311@outlook.com>
2024-05-14 17:27:49 +0700

    update commit

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
-function shuffle(array) {
-  let currentIndex = array.length,
-    randomIndex;
-
-  // While there are elements remaining
-  while (currentIndex !== 0) {
-    // Pick a remaining element
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    // Swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex],
-      array[currentIndex],
-    ];
+const factorial = (n) => {
+  if (n === 0 || n === 1) {
+    return 1;
   }
-
-  return array;
-}
-
-const shuffledDeck = shuffle([1, 2, 3, 4, 5]);
-console.log(shuffledDeck);
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+};
