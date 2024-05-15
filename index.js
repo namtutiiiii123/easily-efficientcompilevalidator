f811e1ff48f6c5a666415a4927f02faaa4b41cb0
namtutiiiii123 <techmedboynam1311@outlook.com>
2024-05-15 18:17:22 +0700

    aptos project

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const factorial = (n) => {
-  if (n === 0 || n === 1) {
-    return 1;
+function partitionLabels(s) {
+  const last = new Array(26);
+  for (let i = 0; i < s.length; i++) {
+    last[s.charCodeAt(i) - "a".charCodeAt(0)] = i;
   }
-  let result = 1;
-  for (let i = 2; i <= n; i++) {
-    result *= i;
+  let anchor = 0;
+  let j = 0;
+  const result = [];
+  for (let i = 0; i < s.length; i++) {
+    j = Math.max(j, last[s.charCodeAt(i) - "a".charCodeAt(0)]);
+    if (i === j) {
+      result.push(i - anchor + 1);
+      anchor = i + 1;
+    }
   }
   return result;
-};
+}
